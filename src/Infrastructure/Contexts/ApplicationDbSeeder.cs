ff8d0211bb4384461720bb7a164ec4c062b13209
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-25 10:40:21 +0500

    Start seeding

---
+++ src/Infrastructure/Contexts/ApplicationDbSeeder.cs
@@ -0,0 +1,46 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using Infrastructure.Identity;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts
+{
+    public class ApplicationDbSeeder
+    {
+        private readonly IMultiTenantContextAccessor<SchoolTenantInfo> _multiTenantContextAccessor;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _applicationdDbContext;
+        public ApplicationDbSeeder(IMultiTenantContextAccessor<SchoolTenantInfo> multiTenantContextAccessor, RoleManager<ApplicationRole> roleManager,
+            UserManager<ApplicationUser> userManager, ApplicationDbContext applicationdDbContext)
+        {
+            _multiTenantContextAccessor = multiTenantContextAccessor;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _applicationdDbContext = applicationdDbContext;
+        }
+
+        public async Task InitializeDatabaseAsync(CancellationToken cancellationToken)
+        {
+            if (_applicationdDbContext.Database.GetMigrations().Any())
+            {
+                if((await _applicationdDbContext.Database.GetPendingMigrationsAsync(cancellationToken)).Any())
+                {
+                    await _applicationdDbContext.Database.MigrateAsync(cancellationToken);
+                }
+                //seeding the data.
+                if(await _applicationdDbContext.Database.CanConnectAsync(cancellationToken))
+                {
+                    //await SeedRolesAsync(cancellationToken);
+                    //await SeedUsersAsync(cancellationToken);
+                }
+            }
+        }
+    }
+}
