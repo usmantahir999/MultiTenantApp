1e4c65968feffb0c0a96b8af7567c501678a438b
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-25 09:51:30 +0500

    constants added

---
+++ src/Infrastructure/Constants/ClaimConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Constants
+{
+    public static class ClaimConstants
+    {
+        public const string Tenant = "tenant";
+        public const string Permission = "permission";
+    }
+}
