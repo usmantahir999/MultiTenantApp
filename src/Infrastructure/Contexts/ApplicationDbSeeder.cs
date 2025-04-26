b6331226fec5a73b7dd4d615b6ecd74628d7bf63
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-26 19:46:15 +0500

    35. Initialize Default Roles - Complete

--- src/Infrastructure/Contexts/ApplicationDbSeeder.cs
+++ src/Infrastructure/Contexts/ApplicationDbSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Finbuckle.MultiTenant.Abstractions;
+using Infrastructure.Constants;
 using Infrastructure.Identity;
 using Infrastructure.Tenancy;
 using Microsoft.AspNetCore.Identity;
@@ -17,13 +18,15 @@ namespace Infrastructure.Contexts
         private readonly RoleManager<ApplicationRole> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _applicationdDbContext;
+        private readonly IMultiTenantContextAccessor<SchoolTenantInfo> _tenantInfoContextAccessor;
         public ApplicationDbSeeder(IMultiTenantContextAccessor<SchoolTenantInfo> multiTenantContextAccessor, RoleManager<ApplicationRole> roleManager,
-            UserManager<ApplicationUser> userManager, ApplicationDbContext applicationdDbContext)
+            UserManager<ApplicationUser> userManager, ApplicationDbContext applicationdDbContext, IMultiTenantContextAccessor<SchoolTenantInfo> tenantInfoContextAccessor)
         {
             _multiTenantContextAccessor = multiTenantContextAccessor;
             _roleManager = roleManager;
             _userManager = userManager;
             _applicationdDbContext = applicationdDbContext;
+            _tenantInfoContextAccessor = tenantInfoContextAccessor;
         }
 
         public async Task InitializeDatabaseAsync(CancellationToken cancellationToken)
@@ -39,6 +42,64 @@ namespace Infrastructure.Contexts
                 {
                     //await SeedRolesAsync(cancellationToken);
                     //await SeedUsersAsync(cancellationToken);
+                }
+            }
+        }
+
+        private async Task InitializeDefaultRolesAsync(CancellationToken ct)
+        {
+            foreach (var roleName in RoleConstants.DefaultRoles)
+            {
+                if (await _roleManager.Roles.SingleOrDefaultAsync(role => role.Name == roleName, ct) is not ApplicationRole incomingRole)
+                {
+                    incomingRole = new ApplicationRole()
+                    {
+                        Name = roleName,
+                        Description = $"{roleName} Role"
+                    };
+
+                    await _roleManager.CreateAsync(incomingRole);
+                }
+
+                if (roleName == RoleConstants.Admin)
+                {
+                    // Assign Admin Permissions
+                    await AssignPermissionsToRoleAsync(SchoolPermissions.Admin, incomingRole, ct);
+
+                    if (_tenantInfoContextAccessor.MultiTenantContext.TenantInfo.Id == TenancyConstants.Root.Id)
+                    {
+                        await AssignPermissionsToRoleAsync(SchoolPermissions.Root, incomingRole, ct);
+                    }
+                }
+                else if (roleName == RoleConstants.Basic)
+                {
+                    // Assign Basic Permissions
+                    await AssignPermissionsToRoleAsync(SchoolPermissions.Basic, incomingRole, ct);
+                }
+            }
+        }
+
+        private async Task AssignPermissionsToRoleAsync(
+            IReadOnlyList<SchoolPermission> incomingRolePermissions,
+            ApplicationRole currentRole,
+            CancellationToken ct)
+        {
+            var currentlyAssignedClaims = await _roleManager.GetClaimsAsync(currentRole);
+
+            foreach (var incomingPermission in incomingRolePermissions)
+            {
+                if (!currentlyAssignedClaims.Any(claim => claim.Type == ClaimConstants.Permission && claim.Value == incomingPermission.Name))
+                {
+                    await _applicationdDbContext.RoleClaims.AddAsync(new ApplicationRoleClaim
+                    {
+                        RoleId = currentRole.Id,
+                        ClaimType = ClaimConstants.Permission,
+                        ClaimValue = incomingPermission.Name,
+                        Description = incomingPermission.Description,
+                        Group = incomingPermission.Group
+                    }, ct);
+
+                    await _applicationdDbContext.SaveChangesAsync(ct);
                 }
             }
         }
--- src/Infrastructure/Tenancy/TenancyConstants.cs
+++ src/Infrastructure/Tenancy/TenancyConstants.cs
@@ -3,5 +3,15 @@
     public class TenancyConstants
     {
         public const string TenantIdName = "tenant";
+        public const string DefaultPassword = "Password@123";
+        public const string FirstName = "Usman";
+        public const string LastName = "Tahir";
+
+        public static class Root
+        {
+            public const string Id = "root";
+            public const string Name = "Root";
+            public const string Email = "admin.root@school.com";
+        }
     }
 }
