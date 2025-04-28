1e74513b10af6f416d7aacf7e8193f9b475143f0
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-28 18:33:21 +0500

    Token implementation started

---
+++ src/core/Application/Features/Identity/Tokens/TokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Identity.Tokens
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ src/core/Application/Features/Identity/Tokens/TokenResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Features.Identity.Tokens
+{
+    public class TokenResponse
+    {
+
+        public string Jwt { get; set; } = default!;
+        public string RefreshToken { get; set; } = default!;
+        public DateTime RefreshTokenExpiryDate { get; set; }
+    }
+}
