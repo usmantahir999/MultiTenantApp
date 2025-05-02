d1dc1a850fb52cb7715cbb7c18d715755a8486db
Usman Tahir <Usman.tahir@synergy-it.com>
2025-05-02 12:49:47 +0500

    added claims extension

---
+++ src/Infrastructure/Identity/ClaimPrincipalExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Constants;
+using System.Security.Claims;
+
+namespace Infrastructure.Identity
+{
+    public static class ClaimPrincipalExtensions
+    {
+        public static string GetEmail(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.Email)!;
+        }
+
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        }
+
+        public static string GetTenant(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimConstants.Tenant)!;
+        }
+
+        public static string GetFirstName(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.Name)!;
+        }
+
+        public static string GetLastName(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.Surname)!;
+        }
+
+        public static string GetPhoneNumber(this ClaimsPrincipal principal)
+        {
+            return principal.FindFirstValue(ClaimTypes.MobilePhone)!;
+        }
+    }
+}
