21758b59d326d9b82c60c86eb85865981e8918e7
Usman Tahir <Usman.tahir@synergy-it.com>
2025-05-05 15:39:15 +0500

    SwaggerHeaderAttributeProcessor

---
+++ src/Infrastructure/OpenApi/SwaggerSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.OpenApi
+{
+    public class SwaggerSettings
+    {
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ContactName { get; set; } = default!;
+        public string ContactEmail { get; set; } = default!;
+        public string ContactUrl { get; set; } = default!;
+        public string LicenseName { get; set; } = default!;
+        public string LicenseUrl { get; set; } = default!;
+    }
+}
