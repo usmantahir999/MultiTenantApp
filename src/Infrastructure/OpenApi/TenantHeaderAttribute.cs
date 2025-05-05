8ecc7c1ade532c949783f95496263941c64eb4be
Usman Tahir <Usman.tahir@synergy-it.com>
2025-05-05 15:12:52 +0500

    Swagger header Attribute

---
+++ src/Infrastructure/OpenApi/TenantHeaderAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Tenancy;
+
+namespace Infrastructure.OpenApi
+{
+    public class TenantHeaderAttribute() 
+        : SwaggerHeaderAttribute 
+        (TenancyConstants.TenantIdName,
+         "Enter the tenant name to access the api",
+         defaultValue :string.Empty,
+         isRequired: true)
+    {
+    }
+}
