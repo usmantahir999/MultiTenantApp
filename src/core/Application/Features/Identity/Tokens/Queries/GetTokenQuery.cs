dda85694139bfb0974c96ef56fa71dc66c52c24a
Usman Tahir <Usman.tahir@synergy-it.com>
2025-05-06 16:25:18 +0500

    get token

---
+++ src/core/Application/Features/Identity/Tokens/Queries/GetTokenQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Identity.Tokens.Queries
+{
+    public class GetTokenQuery : IRequest<IResponseWrapper>
+    {
+        public TokenRequest TokenRequest { get; set; }
+    }
+}
