83251304fa518fbb0b0204e8016e9217c7aa51cd
Usman Tahir <Usman.tahir@synergy-it.com>
2025-05-07 09:45:38 +0500

    token endpoints

---
+++ src/WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _sender = null!;
+        public ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
---
+++ src/WebApi/Controllers/TokenController.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.Identity.Tokens.Queries.Token;
+using Application.Features.Identity.Tokens;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Infrastructure.OpenApi;
+using NSwag.Annotations;
+using Application.Features.Identity.Tokens.Queries.RefreshToken;
+using Infrastructure.Constants;
+using Infrastructure.Identity.Auth;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class TokenController : BaseApiController
+    {
+        [HttpPost("login")]
+        [AllowAnonymous]
+        [TenantHeader]
+        [OpenApiOperation("Used to obtain jwt for login.")]
+        public async Task<IActionResult> GetTokenAsync([FromBody] TokenRequest tokenRequest)
+        {
+            var response = await Sender.Send(new GetTokenQuery { TokenRequest = tokenRequest });
+
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+
+        [HttpPost("refresh-token")]
+        [OpenApiOperation("Used to generate new jwt from refresh token.")]
+        [ShouldHavePermission(action: SchoolAction.RefreshToken, feature: SchoolFeature.Tokens)]
+        public async Task<IActionResult> GetRefreshTokenAsync([FromBody] RefreshTokenRequest refreshTokenRequest)
+        {
+            var response = await Sender.Send(new GetRefreshTokenQuery { RefreshToken = refreshTokenRequest });
+            if (response.IsSuccessful)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+    }
+}
